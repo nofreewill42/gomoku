8373c519aeb988d95e8ac60d416159d57a63d5d5
Ivan, David <avandavad@gmail.com>
2024-04-17 12:33:26 +0200

    feat: competitors main.py

---
+++ src/competition/competitors/Balazs/main.py
@@ -0,0 +1,21 @@
+import random
+from typing import List
+
+from src.state import Action
+from src.state import GameState
+
+
+def take_action(state: GameState, history: List[Action], player: int) -> Action:
+    """overwrite this function.
+
+    Args:
+        state (GameState):
+        history (List[Action]):
+        player (int): current player (1 or -1)
+
+    Returns:
+        Action: the action taken by the player (model).
+    """
+    actions = state.get_possible_actions()
+    action = random.choice(actions)
+    return action
